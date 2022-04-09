1c92e0769c21d08a1d24854a6443f3b22ef0d430
Paisal Patmal <91996742+faisalptml@users.noreply.github.com>
2022-04-09 21:39:39 +0700

    Praktikum 6

---
+++ praktikum 6/controllers/daftar.php
@@ -0,0 +1,13 @@
+<?php
+    class daftar extends CI_Controller {
+        public function __construct() {
+            parent:: __construct() ;
+            $this->load->model('model_daftar') ;
+        }
+
+        public function index() {
+            $data['daftar'] = $this ->model_daftar->getAlldaftar() ;
+            $this -> load->view('daftar') ;
+        }
+    }
+?>
---
+++ praktikum 6/views/welcome_message.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>PRAKTEK 6</title>
+
+</head>
+<body>
+
+<div id="container">
+	<h1>SEMANGAT BELAJAR!</h1>
+
+	<div id="body">
+		<p>Jangan Lupa Berdoa Sebelum Belajar</p>
+
+	</div>
+</div>
+
+</body>
+</html>
